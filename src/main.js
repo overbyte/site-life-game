5db3ec886af866fbef9ceba83248fa37ad499d18
Rory Cullen <rory.cullen@imagination.com>
2018-05-10 12:49:49 +0100

    ADD js

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+const init = () => {
+
+};
+
+
+init();
+
+
+newCtx(sheetSize: Dim, tileSize: Dim, {run:[1, 2, 3, 4]})
+getFrame(state, vel, ctx) : rect
+
+x, y, w, h
+
+state = [run, idle, airborne]
+vel = x y
+ctx
+
